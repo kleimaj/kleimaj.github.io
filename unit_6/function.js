1092a6376441c0fc991b0746b0409a12ba198738
Jacob Kleiman <33333976+kleimaj@users.noreply.github.com>
2019-11-24 13:19:50 -0800

    Unit_6 Complete

---
+++ unit_6/function.js
@@ -0,0 +1,18 @@
+var arr = [
+    // "You win some, you lose some, but you live to fight another day.",
+    // "For relaxing times, make it Suntory time.",
+    "Everything you need is within you.",
+    "Find the flow.",
+    "Be in the present.",
+    "Wait for the moment.",
+    // "Begin.",
+    "The real meditation is how you live your life.",
+    "Be happy in this moment.",
+    "My mama said \"You can't hurry love\"",
+    "Send love to those who need it",
+];
+
+window.onload = function () {
+    var quote = arr[Math.floor(Math.random()*arr.length)];
+    document.getElementById('quote').textContent=quote;
+}
