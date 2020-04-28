67910b790e6e366644d077b6f0acc10ade83862c
jacob kleiman <jacobakleiman@gmail.com>
2020-04-28 11:38:42 -0700

    implements mobile nav

--- src/Layout/Nav/Nav.js
+++ src/Layout/Nav/Nav.js
@@ -5,8 +5,8 @@ import './Nav.css';
 
 const Nav = () => {
     const size = useWindowSize();
-    const render = (size.width > 980) ? renderDesktop() : renderMobile();
     const [show, setShow] = useState(false);
+    const render = (size.width > 980) ? renderDesktop() : renderMobile(show, setShow);
     return render;
 }
 const renderDesktop = () => {
@@ -30,15 +30,34 @@ const renderDesktop = () => {
             </aside>
     )
 }
-const renderMobile = () => {
+const renderMobile = (show, setShow) => {
     return (
         <nav>
             <div className="nav-left">
-            <FontAwesomeIcon icon={faBars} />
+                {show ? 
+                <FontAwesomeIcon icon={faTimes} onClick={() => setShow(false)} />
+                : 
+                <FontAwesomeIcon icon={faBars} onClick={() => setShow(true)} /> }
             </div>
             <div className="nav-center">
         <a href="#"><img id="nav_logo" src="/images/portfolio_icons/LogoDesigner.svg" alt="logo"/></a>
             </div>
+            {/* Hamburger Menu */}
+            {show ?
+            <div className="hamburger_menu">
+                <a className = "hamburger_link" href="colab.html"><span>01:&emsp;FreeChat</span></a>
+                <a className = "hamburger_link" href="jamr.html"><span>02:&emsp;Pikatsume</span></a>
+                <a className = "hamburger_link" href="ircam.html"><span>03:&emsp;Jamr</span></a>
+                <a className = "hamburger_link" href="ircam.html"><span>04:&emsp;Aboop</span></a>
+                <a className = "hamburger_link" href="ircam.html"><span>05:&emsp;Co.Lab</span></a>
+                <a className = "hamburger_link" href="ircam.html"><span>06:&emsp;ircam</span></a>
+                <a className = "hamburger_misc" href="#">About</a>
+                <a className = "hamburger_misc" href="#">Misc</a>
+
+            </div>
+            :
+            ''
+            }
         </nav>
     )
 }
