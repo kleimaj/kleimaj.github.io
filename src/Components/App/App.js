f9a9b983d0cf326eda97f7fd305ca553f63dc5ea
jacob kleiman <jacobakleiman@gmail.com>
2020-04-27 18:58:40 -0700

    adds nav (mobile view)

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,58 +6,6 @@ function App() {
   return (
     <div className="App">
         <Nav />
-      {/* <header>
-            <a href="#"><img id="header_logo" src="Logo.svg" alt="logo"/></a>
-            <aside>
-                <div id="works">
-                <a id="projects" href="index.html">Projects</a>
-                <a className = "project_link" href="colab.html"><span>01:</span>&emsp;Co.Lab</a>
-                <a className = "project_link" href="jamr.html"><span>02:</span>&emsp;Jamr</a>
-                <a className = "project_link" href="ircam.html"><span>03:</span>&emsp;ircam</a>
-                </div>
-                <div id="misc">
-                <a href="profile.html">About</a>
-                <a href="music.html">Misc</a>
-                </div>
-
-
-            </aside>
-            <h2 id="bio"><strong>Jacob Kleiman</strong>
-                is an SEI 
-                student @ 
-                <a href="https://generalassemb.ly">General Assembly</a> in 
-                San Francisco, CA</h2>
-            <img id="jacob" src="images/headshots/me and my mom.png" alt="Meeeee"/>
-        </header>
-        <main>
-            <a className = "card" id = "profile" href="profile.html">
-                <img id = "logo" src="Logo.svg" alt="logo"/>
-                <img id = "palmtree" src="images/portfolio_icons/palm_tree.svg" alt="Palm Tree"/>
-            </a>
-            <div id="desktop_cards">
-            <a className = "card" id = "CoLab" href="colab.html">
-                <div className="card_label">
-                <h3>Co.Lab</h3>
-                <p>UX Project</p>
-                <img className ="icon" src="images/portfolio_icons/group.svg" alt="Co.Lab Icon"/>
-                </div>
-            </a>
-            <a className = "card" id = "Jamr" href="jamr.html">
-                <div className="card_label">
-                <h3>Jamr</h3>
-                <p>Mobile Project</p>
-                <img className ="icon" src="images/portfolio_icons/jamr_logo_white.svg" alt="Jamr Icon"/>
-                </div>
-            </a> 
-            <a className = "card" id = "ircam" href="ircam.html">
-                <div className="card_label">
-                <h3>ircam</h3>
-                <p>Music Classifier</p>
-                <img className ="icon" src="images/portfolio_icons/ircam.svg" alt="Ircam Icon"/>
-                </div>
-            </a>
-            </div>
-        </main> */}
     </div>
   );
 }
--- src/Layout/Nav/Nav.js
+++ src/Layout/Nav/Nav.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
+import './Nav.css';
 
 const Nav = () => {
     const size = useWindowSize();
@@ -7,12 +10,35 @@ const Nav = () => {
 }
 const renderDesktop = () => {
     return (
-        <h1>Desktop</h1>
+        <aside>
+        <a href="#"><img id="header_logo" src="/images/portfolio_icons/LogoDesigner.svg" alt="logo"/></a>
+                <div id="works">
+                <a id="projects" href="index.html"><h2>Projects</h2></a>
+                <a className = "project_link" href="colab.html"><span>01:&emsp;FreeChat</span></a>
+                <a className = "project_link" href="jamr.html"><span>02:&emsp;Pikatsume</span></a>
+                <a className = "project_link" href="ircam.html"><span>03:&emsp;Jamr</span></a>
+                <a className = "project_link" href="ircam.html"><span>04:&emsp;Aboop</span></a>
+                <a className = "project_link" href="ircam.html"><span>05:&emsp;Co.Lab</span></a>
+                <a className = "project_link" href="ircam.html"><span>06:&emsp;ircam</span></a>
+
+                </div>
+                <div id="misc">
+                <a href="profile.html"><h2>About</h2></a>
+                <a href="music.html"><h2>Misc</h2></a>
+                </div>
+            </aside>
     )
 }
 const renderMobile = () => {
     return (
-        <h1>Mobile</h1>
+        <nav>
+            <div className="nav-left">
+            <FontAwesomeIcon icon={faBars} />
+            </div>
+            <div className="nav-center">
+                
+            </div>
+        </nav>
     )
 }
 const useWindowSize = () => {
