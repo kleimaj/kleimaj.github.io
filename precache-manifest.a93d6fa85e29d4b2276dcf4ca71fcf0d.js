5c4e78812499c519e6b3564debfd1326189da2e1
jacob kleiman <jacobakleiman@gmail.com>
2020-05-01 12:31:17 -0700

    Updates

---
+++ precache-manifest.a93d6fa85e29d4b2276dcf4ca71fcf0d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fefe1ec7801b40a7bb9d8f9a60356fdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc97532c52ea89615cb3",
+    "url": "/static/css/2.451bf2e4.chunk.css"
+  },
+  {
+    "revision": "0d2e5258eedcd6b42284",
+    "url": "/static/css/main.2cdbac73.chunk.css"
+  },
+  {
+    "revision": "fc97532c52ea89615cb3",
+    "url": "/static/js/2.3951adea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3951adea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2e5258eedcd6b42284",
+    "url": "/static/js/main.b0fe6275.chunk.js"
+  },
+  {
+    "revision": "30ef98ccc078dcdc8699",
+    "url": "/static/js/runtime-main.13135219.js"
+  },
+  {
+    "revision": "c713fd0df31cf4e5f8e4f09e92698c6d",
+    "url": "/static/media/RAGE.c713fd0d.TTF"
+  },
+  {
+    "revision": "1b6aa711d2c5c6c36a5404a59f462fbf",
+    "url": "/static/media/VulfMonoCode-Light.1b6aa711.otf"
+  },
+  {
+    "revision": "2041c6160263e90a8e1ecaf8d4aafeef",
+    "url": "/static/media/VulfSans-Light.2041c616.otf"
+  },
+  {
+    "revision": "899bf35ffd05371839fce190c266236c",
+    "url": "/static/media/VulfSans-Medium.899bf35f.otf"
+  }
+]);
